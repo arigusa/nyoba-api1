2113b72532de19077acfcf75c5684a519818235d
ari gusa dewa <82886049+arigusa@users.noreply.github.com>
2023-04-08 20:42:04 +0700

    first commit

---
+++ lib/postcard/postCard.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:run/model/model.dart';
+import 'package:run/page/detailPage.dart';
+
+import 'package:run/model/model.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({Key? key, required this.posts}) : super(key: key);
+  final Posts posts;
+
+  // const PostCard({required this.posts});
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: ((context) {
+              return DetailPage(posts: posts);
+            }),
+          ),
+        );
+      },
+      child: Container(
+        decoration: const BoxDecoration(color: Colors.white),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Text(posts.id.toString()),
+            Text(
+              posts.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(posts.body),
+          ],
+        ),
+      ),
+    );
+  }
+}
